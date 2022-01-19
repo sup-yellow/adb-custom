0528fb6db4cf9e201a4933e7bf7989738176d4ac
sup-yellow <97955010+sup-yellow@users.noreply.github.com>
2022-01-19 18:49:20 +0530

    Update addFrame.js

--- addFrame.js
+++ addFrame.js
@@ -10,25 +10,23 @@
     document.querySelector("#chatContainer").insertAdjacentHTML(
       "afterbegin",
       `<div class="mod-head-side-bar-container">
-        <div class="overflow-container-sidebar">
-      <div class="overflow-container-header">
-        <ul class="header-wrapper" style="display: flex;justify-content: space-between;">
-            <div class="header-title" id="main-header-title" style="font-weight: 700; font-size: large;color: black;">TAMI</div>
-            <div style="display:flex;align-items:center;">
-                 <div id="e11" class="icons-box"> 
-                     <span class="header-bar-icon-box" title="minimize">-</span>
-                 </div>
-                 <div id="e10" style="cursor:pointer;"> 
-                 <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;" /></span>
-                 </div>
-                 <div id="e9" class="icons-box"> 
-                     <span class="header-bar-icon-box" title="Close">x</span>
-                 </div>
-            </div>
-        </ul>
-    </div> 
-    </div>
-           </div> `
+         <div class="overflow-container-header">
+               <ul class="header-wrapper" style="display: flex;justify-content: space-between;">
+                  <div class="header-title" id="main-header-title" style="font-weight: 700; font-size: large;color: black;">TAMI</div>
+                   <div style="display:flex;align-items:center;">
+                   <div id="e11" class="icons-box"> 
+                       <span class="header-bar-icon-box" title="minimize">-</span>
+                   </div>
+                   <div id="e10" style="cursor:pointer;"> 
+                   <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;" /></span>
+                   </div>
+                   <div id="e9" class="icons-box"> 
+                       <span class="header-bar-icon-box" title="Close">x</span>
+                   </div>
+              </div>
+             </ul>
+          </div> 
+      </div> `
     );
   } catch (e) {
     console.log("failing while trying to insert add frame script", e);
