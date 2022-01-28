d1637afbbebff9f7251391a34d51da5c08b51992
sup-yellow <97955010+sup-yellow@users.noreply.github.com>
2022-01-28 10:45:14 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -36,7 +36,7 @@ function injectDynamicCssToParent() {
   parentStyles.type = "text/css";
 
   var parentCssStyles =
-    '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; } }';
+    '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; } #fadeshow { display: none;} }';
     
   if (parentStyles.styleSheet) {
     parentStyles.styleSheet.cssText = parentCssStyles;
