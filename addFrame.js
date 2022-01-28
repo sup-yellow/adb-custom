10f948db7ca11c1605f7e81547c5bb4fa0669b7c
sup-yellow <97955010+sup-yellow@users.noreply.github.com>
2022-01-28 09:55:41 +0530

    Update addFrame.js

--- addFrame.js
+++ addFrame.js
@@ -11,17 +11,17 @@
       "afterbegin",
       `<div class="mod-head-side-bar-container">
          <div class="overflow-container-header">
-               <ul class="header-wrapper" style="display: flex;justify-content: space-between;display: flex;flex-direction: row;align-items: center;justify-content: space-between;background-color: rgb(0, 37, 105);padding: 0;list-style: none;height: 4rem;border: 0.5px solid #ccc;border-left: 0;">
-                  <div class="header-title" id="main-header-title" style="font-weight: 400; font-size: medium;color: white;font-family: 'botplatform-font';">
-                  <img class="sidebar-img-icon" src="https://www.chatbotsportal.com/api/chat/get-file/60a74746edb96a69de78af7e" style="width: 37px; height: auto;margin-left: 10px;margin-right: 10px;vertical-align: middle;">TAMI - BOT 2<div style="margin-left: 58px;font-size: small;">TAMI BOT for ADB</div></div>
+<ul class="header-wrapper" style="display: flex;display: flex;flex-direction: row;align-items: center;background-color: rgb(0, 37, 105);padding: 0;list-style: none;height: 4rem;border: 0.5px solid #ccc;border-left: 0;">
+                  <div><img class="sidebar-img-icon" src="https://www.chatbotsportal.com/api/chat/get-file/60a74746edb96a69de78af7e" style="width: 37px; height: auto;margin-left: 10px;margin-right: 10px;vertical-align: middle;"></div><div class="header-title" id="main-header-title" style="font-weight: 400;font-size: medium;color: white;font-family: 'botplatform-font';display: flex;flex-direction: column;margin-left: 0px;">
+                  TAMI - BOT 2<div style="/* margin-left: 58px; */font-size: small;">TAMI BOT for ADB</div></div>
                    <div id="fadeshow">
                    <div id="e11" class="icons-box" style="padding: 0 1rem;"> 
                        <span class="header-bar-icon-box" title="minimize" style="color: white;font-weight: 900;font-size: 30px;width: 100%;padding-left: 0.5rem;cursor: pointer;">-</span>
                    </div>
                    <div id="e10" style="cursor:pointer;"> 
-                   <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;" /></span>
+                   <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;"></span>
                    </div>
-                   <div id="e9" class="icons-box" padding: 0 1rem;> 
+                   <div id="e9" class="icons-box" padding:="" 0="" 1rem;=""> 
                        <span class="header-bar-icon-box" title="Close" style="padding: 0 1rem;color: white;font-weight: 900;font-size: 20px;width: 100%;cursor: pointer;">x</span>
                    </div>
               </div>
